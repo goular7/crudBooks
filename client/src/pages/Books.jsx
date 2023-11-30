d69892ad5bd103e36a79dfe5e7e7001dc8b12691
goular7 <gustavogoulart1970@gmail.com.br>
2023-11-30 13:53:52 -0300

    btnLink

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,24 +1,42 @@
-import React from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
-import axios from 'axios'
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import axios from 'axios';
+import { Link } from "react-router-dom";
 
 const Books = () => {
-    const[books,setBooks] = useState([])
+    const[books,setBooks] = useState([]);
 
     useEffect(()=>{
       const fecthAllBooks = async ()=>{
         try {
-          const res = axios
-        }catch(err){
-          console.log(err)
+          const res = await axios.get("http://localhost:8800/books");
+          setBooks(res.data);
+        }catch(err) {
+          console.log(err);
         }
       }
-    },[])
+      fecthAllBooks();
+    },[]);
 
   return (
-    <div>Books</div>
-  )
-}
+    <div>
+     <h1>Lama Book Shop</h1>
+      <div className="books">
+       {books.map((book) => (
+          <div className="book" key={book.id}>
+            {book.cover && <img src={book.cover} alt="" />}
+            <h2>{book.title}</h2>
+           <p>{book.desc}</p>
+           <span>{book.price}</span>
+         </div>
+        ))}
+      </div>
+      <button>
+        <Link to="/Add">Add new book</Link>
+        </button>
+    </div>
+  );
+};
 
 export default Books
